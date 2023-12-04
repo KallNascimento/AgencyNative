a5b77d7f1a0e194409001231293b4ed47b4f29ec
kallnascimento <iksn7@icloud.com>
2023-12-04 14:49:26 -0300

    Teste com tamagui

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// this provides some helpful reset styles to ensure a more consistent look
 
+// only import this from your web app, not native
+
+import '@tamagui/core/reset.css'
+import { Button, TamaguiProvider, Stack, useTheme, Form, YStack, Input } from 'tamagui'
+import config from './tamagui.config'
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const theme = useTheme()
+  return (
+
+    <TamaguiProvider config={config}>
+      <Stack flex={1} p={15} bg="$blue10">
+        <Form onSubmit={() => alert('Weee')}>
+          <YStack>
+            <Stack p={4}>
+              <Input size="$4" borderWidth={2} placeholder='Login' />
+            </Stack>
+            <Stack p={4}>
+              <Input size="$4" borderWidth={2} placeholder='Senha' />
+            </Stack>
+          </YStack>
+          <Form.Trigger asChild >
+            <Button m={4} bg={'$gray12'} color={'$color.gray1Light'}>Entrar</Button>
+          </Form.Trigger>
+        </Form>
+      </Stack>
+    </TamaguiProvider>
+
+  )
+}
